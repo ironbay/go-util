a458e3d8d8500231935018461cbab0e426f9450f
Dax Ravi <d@ironbay.digital>
2016-04-02 12:50:34 -0400

    Init

---
+++ console/console.go
@@ -0,0 +1,36 @@
+package console
+
+import (
+	"encoding/json"
+	"log"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func JSON(input ...interface{}) {
+	for item := range input {
+		data, err := json.MarshalIndent(item, "", "  ")
+		if err != nil {
+			continue
+		}
+		log.Println(string(data))
+	}
+}
+
+func Print(v ...interface{}) {
+	log.Print(v...)
+}
+
+func Println(v ...interface{}) {
+	log.Println(v...)
+}
+
+func Printf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+func Panic(v ...interface{}) {
+	log.Fatal(v...)
+}
